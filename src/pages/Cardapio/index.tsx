2b99ee9c00debbfdab5d3e2019870a1f2fad761d
Deyvid William <deyvidwms@gmail.com>
2022-09-14 18:03:05 -0300

    Resetando o css e colocando a logo no projeto

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,10 +1,13 @@
 import styles from './Cardapio.module.scss';
 
+// transforma o svg em um componente
+import { ReactComponent as Logo } from "assets/logo.svg";
+
 export default function Cardapio () {
   return (
     <main>
       <nav className={styles.menu}>
-        Aluroni 
+        <Logo />
       </nav>
     </main>
   )
