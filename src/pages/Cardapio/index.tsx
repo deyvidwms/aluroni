e8829114a77e3929d2ee7ae9fde272d6ebd8a50a
Deyvid William <deyvidwms@gmail.com>
2022-09-14 20:38:48 -0300

    Tornando o ordenador funcional

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -12,6 +12,7 @@ import Ordenador from './Ordenador';
 export default function Cardapio () {
   const [busca, setBusca] = useState("");
   const [filtro, setFiltro] = useState<number | null>(null);
+  const [ordenador, setOrdenador] = useState("");
 
   return (
     <main>
@@ -31,7 +32,7 @@ export default function Cardapio () {
         />
         <div className={styles.cardapio__filtros}>
           <Filtros filtro={filtro} setFiltro={setFiltro} />
-          <Ordenador />
+          <Ordenador ordenador={ordenador} setOrdenador={setOrdenador} />
         </div>
       </section>
     </main>
