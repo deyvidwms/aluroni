17244ce4f55f5e3df09cf44a637ef9bb23bbdf07
Deyvid William <deyvidwms@gmail.com>
2022-09-15 06:38:59 -0300

    aplicando filtro por busca e categoria

--- src/pages/Cardapio/Itens/index.tsx
+++ src/pages/Cardapio/Itens/index.tsx
@@ -1,12 +1,37 @@
-import itens from './itens.json';
+import { useEffect, useState } from 'react';
 import Item from './Item';
 
+import cardapio from './itens.json';
 import styles from './Itens.module.scss';
 
-export default function Itens() {
+interface Props {
+  busca: string;
+  filtro: number | null;
+  ordenador: string;
+}
+
+export default function Itens(props: Props) {
+  const [lista, setLista] = useState(cardapio)
+  const { busca, filtro } = props;
+  
+  function testaBusca(title: string) {
+    const regex = new RegExp(busca, 'i');
+    return regex.test(title);
+  }
+
+  function testaFiltro(id: number) {
+    if(filtro !== null) return filtro === id;
+    return true;
+  }
+
+  useEffect( () => {
+    const novaLista = cardapio.filter(item => testaBusca(item.title) && testaFiltro(item.category.id));
+    setLista(novaLista);
+  }, [busca, filtro] );
+  
   return (
     <div className={styles.itens}>
-      { itens.map( item => (
+      { lista.map( item => (
         <Item 
           key={item.id} 
           {...item}
