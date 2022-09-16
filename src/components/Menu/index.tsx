9ea6b67855f1b5765964b338d27e5d0390694224
Deyvid William <deyvidwms@gmail.com>
2022-09-16 13:54:16 -0300

    Utilizando Link para não recarregar a pagina

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -3,6 +3,8 @@ import { ReactComponent as Logo } from 'assets/logo.svg';
 
 import styles from './Menu.module.scss';
 
+import { Link } from 'react-router-dom';
+
 export default function Menu() {
   const rotas = [{
     label: 'Início',
@@ -21,9 +23,9 @@ export default function Menu() {
       <ul className={styles.menu__list}>
         {rotas.map((rota, index) => (
           <li key={index} className={styles.menu__link}>
-            <a href={rota.to}>
+            <Link to={rota.to}>
               {rota.label}
-            </a>
+            </Link>
           </li>
         ))}
       </ul>
