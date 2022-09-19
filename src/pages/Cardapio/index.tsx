2a1e25ba5fe5d5b945a49f898c889be342e13aff
Deyvid William <deyvidwms@gmail.com>
2022-09-19 12:35:03 -0300

    Alterando rota e criando componente header

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -13,24 +13,17 @@ export default function Cardapio () {
   const [ordenador, setOrdenador] = useState('');
 
   return (
-    <main>
-      <header className={styles.header}>
-        <div className={styles.header__text}>
-          A cada do código e da massa
-        </div>
-      </header>
-      <section className={styles.cardapio}>
-        <h3 className={styles.cardapio__titulo}>Cardapio</h3>
-        <Buscador 
-          busca={busca} 
-          setBusca={setBusca} 
-        />
-        <div className={styles.cardapio__filtros}>
-          <Filtros filtro={filtro} setFiltro={setFiltro} />
-          <Ordenador ordenador={ordenador} setOrdenador={setOrdenador} />
-        </div>
-        <Itens busca={busca} filtro={filtro} ordenador={ordenador} />
-      </section>
-    </main>
+    <section className={styles.cardapio}>
+      <h3 className={styles.cardapio__titulo}>Cardapio</h3>
+      <Buscador 
+        busca={busca} 
+        setBusca={setBusca} 
+      />
+      <div className={styles.cardapio__filtros}>
+        <Filtros filtro={filtro} setFiltro={setFiltro} />
+        <Ordenador ordenador={ordenador} setOrdenador={setOrdenador} />
+      </div>
+      <Itens busca={busca} filtro={filtro} ordenador={ordenador} />
+    </section>
   );
 }
--- src/routes.js
+++ src/routes.js
@@ -4,15 +4,18 @@ import Cardapio from 'pages/Cardapio';
 import Menu from 'components/Menu';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import PaginaPadrao from 'components/PaginaPadrao';
 
 export default function AppRouter() {
   return (
-    <Router>
-      <Menu />
-      <Routes>
-        <Route path='/' element={<Inicio />} />
-        <Route path='/cardapio' element={<Cardapio />} />
-      </Routes>
-    </Router>
+    <main>
+      <Router>
+        <Menu />
+        <Routes path='/' element={<PaginaPadrao />}>
+          <Route index element={<Inicio />} />
+          <Route path='cardapio' element={<Cardapio />} />
+        </Routes>
+      </Router>
+    </main>
   );
 }
